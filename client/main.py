4eb48a87ab48af8c7bf6bf6751d1de1178283002
Theucalyptus <28013186+Theucalyptus@users.noreply.github.com>
2025-06-24 14:44:51 +0200

    feat(client): use multiple networks (main+backup)

--- client/main.py
+++ client/main.py
@@ -14,8 +14,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # networking queues and buffers
-q_net_out, q_net_in = Queue(), Buffer()
-
+q_netMain_out, q_netMain_in = Queue(), Buffer()
+q_netAlt_out, q_netAlt_in = Queue(), Buffer()
 
 ## SENSORS 
 sensorsList = []
@@ -37,24 +37,36 @@ if config.getboolean('sensors', 'canbus'):
 ### Object Detection
 if config.getboolean('sensors', 'camera'):
     cam = Camera()
-    t = threading.Thread(target=cam.ObjectDetection, args=(q_net_in,))
+    t = threading.Thread(target=cam.ObjectDetection, args=(q_netMain_in,))
     t.start()
 
 
-## NETWORK
-int = config.get('network', 'interface')
-if int == "uart":
-    ### UART (LoRa)
-    uart_service = network.UartService(q_net_in, q_net_out)
-    uart_service.run()
-elif int == "http":
-    ### HTTP (WiFi, Ethernet, LTE/5G)
-    http_service = network.HTTPService(q_net_in, q_net_out)
-    http_service.run()
-else:
-    print("config error: invalide network interface")
-    exit(1)
 
+## NETWORK
+MAIN_NET = None
+ALT_NET = None
+def __enableNetInterface(interface, q_in, q_out):
+    if interface != '':
+        if interface == "uart":
+            ### UART (LoRa)
+            uart_service = network.UartService(q_in, q_out)
+            uart_service.run()
+            return uart_service
+        elif interface == "http":
+            ### HTTP (WiFi, Ethernet, LTE/5G)
+            http_service = network.HTTPService(q_in, q_out)
+            http_service.run()
+            return http_service
+        else:
+            print("config error: invalide network interface:", interface)
+            exit(1)
+    else:
+        return None
+
+main = config.get('network', 'main_interface')
+alternative = config.get('network', 'alt_interface')
+MAIN_NET = __enableNetInterface(main, q_netMain_in, q_netMain_out)
+ALT_NET = __enableNetInterface(alternative, q_netAlt_in, q_netAlt_out)
 
 message = {'device-id':config.get('general', 'device_id'), 'type':1}
 
@@ -66,8 +78,14 @@ def __sendWorker():
         # only send a new status update if latest sensor data changed (except timestamp)
         if lastSent != message:
             lastSent = message.copy()
-            q_net_in.put(lastSent)
-            
+            if MAIN_NET.isUp:
+                q_netMain_in.put(lastSent)
+            elif ALT_NET != None and ALT_NET.isUp:
+                q_netAlt_in.put(lastSent)
+            else:
+                logger.info("all networks are down. the data is still logged localy.")
+
+
 threading.Thread(target=__sendWorker).start()
 
 ## Polling all sensors and update the dict containing all the information
